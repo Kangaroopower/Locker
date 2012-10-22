da570cedf2a43db7d0ae54eb72acc63c4250de8a
Kangaroopower <racecar360@gmail.com>
2012-10-21 21:05:06 -0700

    lots of bugfixes, allow to monitor reads
    plus change the version type to mockingjay. That sounds cooler than
    guinea pig... I mean who would read the hunger games if Katniss's pin
    was a GUINEA PIG?!?!?? :P

--- Locker.js
+++ Locker.js
@@ -1,11 +1,10 @@
 //Locker- File API library
 //CURENTLY IN ALPHA
 (function	(window, undefined) {
-
 	//define some variables
 	var document = window.document,
 		Locker = {
-			version: '1.0 Guinea Pig'
+			version: '1.0 Mockingjay'
 		};
 
 	Locker.fileSelect = function (e) {
@@ -18,26 +17,26 @@
 				'lastmodified': f.lastModifiedDate,
 				'obj': f
 
-			}
+			};
 		}
 		return false;
 	};
 
-	Locker.handleDrag = function (e) {
+	var handleDrag = function (e) {
 		e.stopPropagation();
 		e.preventDefault();
 		e.dataTransfer.dropEffect = 'copy';
 	};
 
-	Locker.drag = function (dropZone)
-		dropZone.addEventListener('dragover', Locker.dragOver, false);
-		dropZone.addEventListener('drop', Locker.fileSelect, false);
+	Locker.drag = function (dropZone, ondrop) {
+		dropZone.addEventListener('dragover', handleDrag, false);
+		dropZone.addEventListener('drop', ondrop, false);
 	};
 
 	Locker.image = function (e) {
 		var f = Locker.fileSelect(e).obj;
 		if (!f.type.match('image.*')) {
-			break;
+			return false;
 		}
 
 		var reader = new FileReader();
@@ -46,7 +45,7 @@
 			return {
 				'file': theFile,
 				'name': escape(theFile.name)
-			}
+			};
 		})(f);
 	};
 
@@ -59,16 +58,16 @@
 
 		var file = files[0], reader = new FileReader();
 		start = parseInt(start) || 0;
-		stop = parseInt(stopByte) || file.size - 1;
+		stop = parseInt(stop) || file.size - 1;
 
 		reader.onloadend = function(e) {
-			if (e.target.readyState == FileReader.DONE) {
+			if (e.target.readyState === FileReader.DONE) {
 				return {
 					'content': reader.readAsBinaryString(file.slice(start, stop + 1)),
 					'start': start + 1,
 					'end': stop + 1,
 					'size': file.size
-				}
+				};
 			}
 		};
 	};
@@ -93,5 +92,41 @@
 		return reader.readAsArrayBuffer(file);
 	};
 
+	Locker.monitor = function (e, start, progress, abortel) {
+		var file = Locker.fileSelect(e).obj, reader = new FileReader();
+		reader.onerror = function (evt) {
+			switch(evt.target.error.code) {
+				case evt.target.error.NOT_FOUND_ERR:
+					alert('File Not Found!');
+					break;
+				case evt.target.error.NOT_READABLE_ERR:
+					alert('File is not readable');
+					break;
+				case evt.target.error.ABORT_ERR:
+					break; // noop
+				default:
+					alert('An error occurred reading this file.');
+			}
+		};
+		reader.onprogress = function(e) {
+			eval('progress(e);');
+		};
+		reader.onabort = function(e) {
+			alert('File read cancelled');
+		};
+		reader.onloadstart = function (e) {
+			eval('start(e);');
+		};
+		reader.onload = function (e) {
+			eval('ondone(e);');
+		};
+
+		document.getElementById(abortel).onclick = reader.abort();
+
+		// Read in the image file as a binary string.
+		reader.readAsBinaryString(file[0]);
+	};
+
 	window.Locker = Locker;
+
 })(window, undefined);
