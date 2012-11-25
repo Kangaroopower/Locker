1f9be68121c3b744f92185fec3801f42cf8c85ca
Kangaroopower <racecar360@gmail.com>
2012-11-24 19:57:53 -0800

    quick fix

--- Locker.js
+++ Locker.js
@@ -1,15 +1,18 @@
-(function (w) {
-	/*** BASIC STORAGE ***/
-	var rPrefix = /^__locker__/;
-	var store = Locker = function(key) {
+(function (w, ns) {
+	
+	/*** LOCKER STORAGE FUNCTIONS ***/
+
+	/** Storage type creation functions **/
+	var rPrefix = /^__locker__/, store = {};
+	ns.store = function(key, info) {
 		var type = store.type.name;
-		if (w[type] == undefined) {
-			checkNext(key, value, type.prio)
+		if (type == null) {
+			checkNext(key, info, type.prio);
 		}
 
-		var act {
-			store: function (value) {
-				return store.types[type].value(key, value);
+		var act = {
+			store: function () {
+				return store.types[type].value(key, info);
 			},
 			get: function () {
 				return store.types[type].value(key, null, 'get');
@@ -21,7 +24,7 @@
 				var name;
 				if (onlyprefixed === true) {
 					for (name in w.localStorage) {
-						if (-1 !== name.indexOf(rPrefix))
+						if (-1 !== name.indexOf(rPrefix)) {
 							store.types[type].value(name , null, 'remove');
 						}
 					}
@@ -31,9 +34,9 @@
 					}
 				}
 			},
-			storeObj: function (value) {
+			storeObj: function () {
 				if (typeof JSON != undefined && JSON.stringify) {
-					return store.types[type].value(key, JSON.stringify(value));
+					return store.types[type].value(key, JSON.stringify(info));
 				} else {
 					return false;
 				}
@@ -46,36 +49,35 @@
 				}
 			}
 		};
-		return store.types[type].value(key, value);
+		return act;
 	};
 
 	store.types = {};
 	store.type = {
 		name: null,
 		prio: -1
-	}
+	};
 	store.addType = function(type, storage, priority) {
-		if (!store.type.name || priority > store.type.prio) {
+		if (!store.type.name || (priority && priority > store.type.prio)) {
 			store.type = {
 				type: type,
 				prio: priority
-			}
+			};
 		}
 
 		store.types[type] = {
 			value: storage,
 			prio: priority
-		} 
+		};
 		store[type] = function(key, value) {
-			return store(key, value, type);
+			return ns.store(key, value, type);
 		};
 	};
 
 	function checkNext (key, value, prio) {
 		for (var i in store.types) {
-			var down = prio--;
-			if (store.types[i].prio === down) {
-				store(key, value, store.types[i].name);
+			if (store.types[i].prio === --prio) {
+				ns.store(key, value, store.types[i].name);
 			}
 		}
 	}
@@ -100,7 +102,7 @@
 					store[storageType]();
 					try {
 						storage.setItem(key, value);
-					} catch(error) {
+					} catch(err) {
 						throw "locker store quota exceeded";
 					}
 				}
@@ -119,7 +121,7 @@
 			var randnum = Math.Random();
 			w[type].setItem("__locker"+ randnum +"__", "x");
 			w[type].removeItem("__locker"+ randnum +"__");
-			createStorageType(webStorageType, w[webStorageType], 1);
+			createStorageType(type, w[type], 1);
 		} catch(e) {}
 	}
 
@@ -127,7 +129,7 @@
 	if (!store.types.localStorage && w.globalStorage) {
 		// try/catch for file protocol in Firefox
 		try {
-			createStorageType("globalStorage", w.globalStorage[w.location.hostname], 1);
+			createStorageType("globalStorage", w.globalStorage[w.location.hostname], 0);
 			// Default to globalStorage in Firefox 2.0 and 3.0
 			if (store.type === "sessionStorage") {
 				store.type = "globalStorage";
@@ -135,5 +137,74 @@
 		} catch(e) {}
 	}
 
-	w.Locker = Locker
-})(window);
+
+	/*** LOCKER SORTING FUNCTIONS ***/
+
+	//Compares two numbers to see which one's larger 
+	function compare(a, b) {
+		if (a > b) {
+			return 1;
+		} else if (a === b) {
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+	
+	//Creates and returns a randomly generated array of integers or strings
+	function randomArray(size, range, str) {
+		var data = [];
+		if (typeof size !== "number" || size < 1) {
+			size = 1000;
+		}
+		if (typeof range !== "number" || range < 1) {
+			range = 10000;
+		}
+		var i = -1;
+		while (++i < size) {
+			data[i] = Math.floor(Math.random() * range + 1);
+		}
+		if (str) {
+			i = -1;
+			while (++i < size) {
+				data[i] = "" + data[i];
+			}
+		}
+		return data;
+	}
+	
+	//Core logic behind merge sort
+	function merge(left, right){
+		var result  = [],
+			il = 0,
+			ir = 0;
+
+		while (il < left.length && ir < right.length){
+			if (left[il] < right[ir]){
+				result.push(left[il++]);
+			} else {
+				result.push(right[ir++]);
+			}
+		}
+
+		return result.concat(left.slice(il)).concat(right.slice(ir));
+	}
+
+	//Sorts an array with merge sort.
+	function mergeSort(items){
+		if (items.length < 2) {
+			return items;
+		}
+
+		var middle = Math.floor(items.length / 2),
+			left = items.slice(0, middle),
+			right = items.slice(middle);
+
+		return merge(mergeSort(left), mergeSort(right));
+	}
+
+	// expose public members
+	ns.compare = comparator;
+	ns.sort = mergeSort;
+	ns.randomArray = randomArray;
+})(window, window.Locker = window.Locker || {version: '1.0 Alpha'});
